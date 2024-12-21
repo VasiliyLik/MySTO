6e118842f0deefacb848d5c1a1fa952602781661
VasiliyLik <vasya_li@mail.ru>
2024-12-21 21:35:51 +0300

    added detail and delete features

--- app/src/main/java/com/likchachevskiy/android/mysto/ui/screens/listcar/ListCarFragment.kt
+++ app/src/main/java/com/likchachevskiy/android/mysto/ui/screens/listcar/ListCarFragment.kt
@@ -6,25 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.likchachevskiy.android.mysto.R
-import com.likchachevskiy.android.mysto.adapter.CarsAdapter
+import com.likchachevskiy.android.mysto.ui.adapter.CarsAdapter
 import com.likchachevskiy.android.mysto.databinding.FragmentCarListBinding
+import com.likchachevskiy.android.mysto.domain.entity.Car
+import com.likchachevskiy.android.mysto.utilits.APP_ACTIVITY
 
-class CarListFragment : Fragment(R.layout.fragment_car_list) {
+class ListCarFragment : Fragment(R.layout.fragment_car_list) {
 
     private var _binding: FragmentCarListBinding? = null
     private val binding get() = _binding!!
-
-//    private val carViewModel: CarViewModel by activityViewModels()
 
     private lateinit var adapter: CarsAdapter
 
     private lateinit var recyclerView: RecyclerView
 
-//    private var carList = mutableListOf<Car>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,15 +31,12 @@ class CarListFragment : Fragment(R.layout.fragment_car_list) {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentCarListBinding.inflate(inflater, container, false)
-        val view = binding.root
-
-
 
         binding.btnAddCar.setOnClickListener {
             findNavController().navigate(R.id.action_carListFragment_to_addCarFragment)
         }
 
-        return view
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -66,8 +62,18 @@ class CarListFragment : Fragment(R.layout.fragment_car_list) {
         }
     }
 
-override fun onDestroyView() {
-    super.onDestroyView()
-    _binding = null
-}
+    companion object {
+
+        fun onClickCar(car: Car) {
+            val bundle = Bundle()
+            bundle.putParcelable("car", car)
+            APP_ACTIVITY.findNavController(R.id.nav_host_fragment)
+                .navigate(R.id.action_carListFragment_to_detailCarFragment, bundle)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
